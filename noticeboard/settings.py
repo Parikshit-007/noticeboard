36999ed87ab60ebfb41326babcc0fea0f5611ced
Parikshit07 <prkshtpandey12@gmail.com>
2024-03-27 10:16:56 +0530

    vercel changes

--- noticeboard/settings.py
+++ noticeboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3)bigi@1+z#6zlx!w)+f1qif3#)^9dm9wfcjqw@(d*-hpl5=89'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
 
 
 # Application definition
